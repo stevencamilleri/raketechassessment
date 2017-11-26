9198ef39947190ee29453f75df56cc5f21d96a1b
stevencamilleri <stevencamilleri96@gmail.com>
2017-11-26 16:39:22 +0100

    Header & Footer Implimentation

---
+++ src/wp-content/themes/stevencamilleri/footer.php
@@ -0,0 +1,91 @@
+<footer>
+
+	<!-- <script type="text/javascript" src="custom.js"></script> -->
+
+	<!-- FOOTER SECTION -->
+
+	<div class="container">
+		<div class="row footer">
+			<div class="col-md company-info-col">
+				<div class="company-info-footer">
+					<a class="logo"><img src="wp-content/themes/stevencamilleri/img/logo.png" width="40px" height="40px"></a>
+					<div class="logo-text">
+						<a class="navbar-brand" href="#">Company Name</a>
+						<span class="brand-desc">Tagline Goes Here</span>
+					</div>
+				</div>
+				<div class="social-networks">
+						<ul class="social-icons">
+					    <li>
+					      <a href="" class="social-icon start facebook"> <i class="fa fa-facebook"></i></a>
+					    </li>
+					    <li>
+					      <a href="" class="social-icon twitter"> <i class="fa fa-twitter"></i></a>
+					    </li>
+					    <li>
+					      <a href="" class="social-icon pinterest"> <i class="fa fa-pinterest"></i></a>
+					    </li>
+					    <li>
+					      <a href="" class="social-icon linkedin end"> <i class="fa fa-linkedin"></i></a>
+					    </li>
+				  </ul>
+				</div>
+				<div class="footer-text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+				</div>
+			</div>
+			<div class="col-md latest-posts">
+				<h1>Latest Posts</h1>
+				<ul class="latest-posts-ul">
+					<li>
+						<div class="featured-image">
+						</div>
+						<div class="post-text">
+							<h3>Lorem ipsum dolor sit amet cons...</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+					</li>
+					<li>
+						<div class="featured-image">
+						</div>
+						<div class="post-text">
+							<h3>Lorem ipsum dolor sit amet cons...</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+					</li>
+					<li>
+						<div class="featured-image">
+						</div>
+						<div class="post-text">
+							<h3>Lorem ipsum dolor sit amet cons...</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<!-- FOOTER COPYRIGHT SECTION -->
+
+	<div class="container-fluid copyright-container">
+		<div class="container">
+			<div class="row copyright-row">
+				<div class="col-md col-sm-12 copyright-col">
+					<span>Terms of Use</span>
+					<span>&nbsp;|&nbsp;</span>
+					<span>Privacy & Cookies</span>
+				</div>
+				<div class="col-md-6 col-sm-12 copyright-col-2">
+					<span>Copyright &copy; 2016, All Rights Reserved.</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	</footer>
+
+	<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ src/wp-content/themes/stevencamilleri/header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+	<head>
+		<title><?php bloginfo('name'); ?><?php wp_title('|'); ?></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<?php wp_head(); ?>
+	</head>
+
+	<body>
+
+<!-- HEADER SECTION -->
+
+		<div class="separator-orange">
+		</div>
+
+		<div class="container-fluid rt-header">
+
+			<!-- NAVIGATION SECTION -->
+
+			<div class="container">
+				<div class="row rt-header-row">
+						<nav class="navbar navbar-inverse">
+							<div class="company-info-header">
+								<a class="logo"><img src="wp-content/themes/stevencamilleri/img/logo.png" width="40px" height="40px"></a>
+								<div class="logo-text">
+									<a class="navbar-brand" href="#">Company Name</a>
+									<span class="brand-desc">Tagline Goes Here</span>
+								</div>
+							</div>
+							<a id="nav-toggle" href="#"><span></span></a>
+						</nav>
+				</div>
+			</div>
+
+			<!-- TITLE SECTION -->
+			<div class="container">
+				<div class="row">
+					<div class="col-md header-title">
+						<div class="header-text w3-animate-zoom">
+							<span class="hello">Hello</h1>
+							<h1>We Are Online Clinic</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
