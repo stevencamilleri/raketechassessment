3cd66de2d9c91619114be94f0b49df0c14c04562
stevencamilleri <stevencamilleri96@gmail.com>
2017-11-26 16:42:13 +0100

    Site Page Content

---
+++ src/wp-content/themes/stevencamilleri/index.php
@@ -0,0 +1,155 @@
+<?php get_header(); ?>
+
+<!-- ABOUT US SECTION -->
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 about-us">
+				<h1>About Us</h1>
+				<br>
+				<div class="separator"></div>
+				<br>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+		</div>
+
+		<!-- ICONS SECTION -->
+
+		<div class="row about-us-icons">
+				<div class="col-md-4 icon-col">
+					<div class="icon-box">
+						<img src="wp-content/themes/stevencamilleri/img/icon-1.png">
+					</div>
+					<div class="icon-text">
+						<p>Lorem ipsum dolor<br> sit amet</p>
+					</div>
+				</div>
+				<div class="col-md-4 icon-col">
+					<div class="icon-box-2">
+						<img src="wp-content/themes/stevencamilleri/img/icon-2.png">
+					</div>
+					<div class="icon-text">
+						<p>Lorem ipsum dolor<br> sit amet</p>
+					</div>
+				</div>
+				<div class="col-md-4 icon-col">
+					<div class="icon-box">
+						<img src="wp-content/themes/stevencamilleri/img/icon-3.png">
+					</div>
+					<div class="icon-text">
+						<p>Lorem ipsum dolor<br> sit amet</p>
+					</div>
+				</div>
+		</div>
+	</div>
+
+<!-- BROWSER SECTION -->
+
+	<div class="container-fluid browser-container">
+		<div class="row">
+			<div class="col-lg-5 col-md-8 col-sm-8 browser-text w3-animate-zoom">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+		</div>
+	</div>
+
+<!-- TESTIMONIALS SECTION -->
+
+	<div class="container">
+		<div class="row testimonials-row">
+			<div class='col-md-8 testimonials-col'>
+      <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+
+        <!-- BOTTOM CAROUSEL INDICATORS -->
+        <ol class="carousel-indicators">
+          <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#quote-carousel" data-slide-to="1"></li>
+          <li data-target="#quote-carousel" data-slide-to="2"></li>
+        </ol>
+
+				<!-- CAROUSEL SLIDE QUOTES -->
+			 <div class="carousel-inner" role="listbox">
+
+				 <!-- QUOTE 1 -->
+				 <div class="carousel-item active">
+					 <blockquote>
+						 <div class="row">
+							 <div class="col-sm-12 text-center">
+								 <img class="img-circle" src="wp-content/themes/stevencamilleri/img/profile.png" style="width: 130px; height:130px;">
+							 </div>
+							 <div class="col-sm-12">
+								 <span class="profile-name">Alice G. Mullins</span>
+								 <span class="profile-job">General Physician</span>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							 </div>
+						 </div>
+					 </blockquote>
+				 </div>
+
+				<!-- QUOTE 2 -->
+				<div class="carousel-item">
+					<blockquote>
+						<div class="row">
+							<div class="col-sm-12 text-center">
+								<img class="img-circle" src="wp-content/themes/stevencamilleri/img/profile.png" style="width: 130px; height:130px;">
+							</div>
+							<div class="col-sm-12">
+								<span class="profile-name">Alice G. Mullins</span>
+								<span class="profile-job">General Physician</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							</div>
+						</div>
+					</blockquote>
+				</div>
+
+				<!-- QUOTE 3 -->
+				 <div class="carousel-item">
+					 <blockquote>
+						 <div class="row">
+							 <div class="col-sm-12 text-center">
+								 <img class="img-circle" src="wp-content/themes/stevencamilleri/img/profile.png" style="width: 130px; height:130px;">
+							 </div>
+							 <div class="col-sm-12">
+								 <span class="profile-name">Alice G. Mullins</span>
+								 <span class="profile-job">General Physician</span>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							 </div>
+						 </div>
+					 </blockquote>
+				 </div>
+
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- BULLETS SECTION -->
+
+	<div class="container-fluid bullet-points-container">
+		<div class="container">
+			<div class="row bullet-points-row">
+				<div class="col-md bullet-points">
+					<h1>Lorem ipsum dolor sit amet</h1>
+					<span class="bullet-desc">Sed do eiusmod tempor</span>
+					<ul class="bullet-text">
+						<li>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit volupta accusantium doloremque laudantium.</p>
+						</li>
+						<br>
+						<li>
+							<p>Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae. </p>
+						</li>
+						<br>
+						<li>
+							<p>Nemo enim ipsam voluptatem quia voluptas sit aspern atur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md bullet-points-img">
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
